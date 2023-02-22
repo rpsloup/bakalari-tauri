537f98da019aba662e095cb256e2c918cb1793f1
rpsloup <rpsloup@proton.me>
2023-02-22 08:12:43 +0100

    Adding the login form

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,31 @@
-const LoginPage = (): JSX.Element => (
-  <>
-    <h1>Login</h1>
-  </>
-);
+import type { FormEvent } from 'react';
+
+const LoginPage = (): JSX.Element => {
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="endpoint">Endpoint</label>
+        <br />
+        <input type="text" name="endpoint" />
+        <br />
+        <label htmlFor="username">Username</label>
+        <br />
+        <input type="text" name="username" />
+        <br />
+        <label htmlFor="password">Password</label>
+        <br />
+        <input type="password" name="password" />
+        <br />
+        <br />
+        <input type="submit" value="Sign in" />
+      </form>
+    </>
+  );
+};
 
 export default LoginPage;
